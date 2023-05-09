a9efb680c68912371adc44b3c5fabacab952930f
sou <soouaho@gmail.com>
2023-05-09 06:45:20 -0700

    minigame

--- Q4GameProject/Assets/Celeste/Scripts/HoldDown.cs
+++ Q4GameProject/Assets/Celeste/Scripts/HoldDown.cs
@@ -16,16 +16,21 @@ public class HoldDown : MonoBehaviour
     public GameObject obj2;
     public Animator anim;
     public Timer timer;
+    public GameObject water;
+    public GameObject clear;
+    public GameObject ui;
+    //public GameObject button1;
+    //public GameObject button2;
+    //public GameObject button3;
+    //public GameObject button4;
 
     void Start()
     {
-        obj.SetActive(false);
-        obj2.SetActive(false);
+        
     }
 
     void Update()
     {
-        Timer += Time.deltaTime;
 
     }
 
@@ -33,42 +38,82 @@ public class HoldDown : MonoBehaviour
     {
         obj2.SetActive(true);
         isPressed = true;
-        if (WaterPercent == 0)
-        {
-            percent.text = "0%";
-        }
 
-        if (Timer>= DelayAmount)
+        if (WaterPercent <= 4)
         {
             WaterPercent++;
+            if (WaterPercent == 0)
+            {
+                percent.text = "0%";
+            }
+            
+            if (WaterPercent == 1)
+            {
+                percent.text = "25%";
+                water.SetActive(true);
+            }
+            if (WaterPercent == 2)
+            {
+                percent.text = "50%";
+                anim.SetBool("Water", true);
+                water.SetActive(false);
+            }
+            if (WaterPercent == 3)
+            {
+                percent.text = "75%";
+                anim.SetBool("Close", true);
+                anim.SetBool("Water", false);
+            }
         }
-        if (WaterPercent == 1)
-        {
-            percent.text = "25%";
-        }
-        if (WaterPercent == 2)
-        {
-            percent.text = "50%";
-            //anim.SetBool("Water", true);
-        }
-        if (WaterPercent == 3)
-        {
-            percent.text = "75%";
-            //anim.SetBool("Close", true);
-        }
+
     }
     public void OnMouseUp()
     {
         isPressed = false;
         if (WaterPercent == 4)
         {
-            //anim.SetBool("Open", true);
+            obj.SetActive(false);
+            ui.SetActive(false);
+            clear.SetActive(true);
+            anim.SetBool("Open", true);
+            anim.SetBool("Close", false);
             Debug.Log("Perfect Noodles You Win!!!");
             percent.text = "100%";
-            obj.SetActive(true);
             timer.Invoke("Return", 2f);
             PlayerPrefs.SetInt("FoodMiniGameComplete", 1);
         }
     }
+
+    //public void PercentUp()
+    //{
+    //    WaterPercent++;
+    //}
+
+    //public void Button1()
+    //{
+    //    button1.SetActive(false);
+    //    button2.SetActive(true);
+    //    WaterPercent++;
+    //}
+
+    //public void Button2()
+    //{
+    //    button2.SetActive(false);
+    //    button3.SetActive(true);
+    //    WaterPercent++;
+    //}
+
+    //public void Button3()
+    //{
+    //    button3.SetActive(false);
+    //    button4.SetActive(true);
+    //    WaterPercent++;
+    //}
+
+    //public void Button4()
+    //{
+    //    button4.SetActive(false);
+    //    WaterPercent++;
+    //}
 
 }
